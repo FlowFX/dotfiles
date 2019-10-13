e14fb1e0827b4d5b3f8f042795382eb17d9e4942
Florian Posdziech <hallo@flowfx.de>
2019-10-13 14:59:43 +0200

    [Xorg] limit x keybindings to debian installs

--- zsh/configs/keymappings.zsh
+++ zsh/configs/keymappings.zsh
@@ -3,7 +3,13 @@
 #
 # This is OS dependant. For macOS, I use Karabiner.
 # cf. https://stackoverflow.com/a/8597411
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  setxkbmap -option "caps:ctrl_modifier"
-  xcape -e "Caps_Lock=Escape"
+#
+# On Arch, I don't use X at all.
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+
+  if [[ "$OSTYPE" == "linux-gnu" && "$ID" == "debian" ]]; then
+    setxkbmap -option "caps:ctrl_modifier"
+    xcape -e "Caps_Lock=Escape"
+  fi
 fi
