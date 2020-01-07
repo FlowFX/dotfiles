556bf5133bbbc8e9cfeb5970a8648ec800a4506b
Florian Posdziech <hallo@flowfx.de>
2020-01-07 19:23:03 +0100

    [offlineimap] Use more generic pass command

--- config/offlineimap/offlineimap.py
+++ config/offlineimap/offlineimap.py
@@ -2,7 +2,12 @@
 
 import commands
 
-def get_pass(account=None, server=None):
-    command = "pass show Email/mailbox.org"
+def get_pass(server=None):
+    params = {
+        'command': 'show',
+        'server': server
+    }
+
+    command = "pass %(command)s Email/%(server)s" % params
     return commands.getoutput(command)
 
